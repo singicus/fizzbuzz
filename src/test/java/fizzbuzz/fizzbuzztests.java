4829acd6e6fa897621d0e5574d7d6e43214d6ffc
Sandy Arendt <sandra@webcattechnology.com>
2017-01-11 17:08:45 -0500

    refactored whitespaces

--- src/test/java/fizzbuzz/fizzbuzztests.java
+++ src/test/java/fizzbuzz/fizzbuzztests.java
@@ -4,27 +4,25 @@ import org.junit.*;
 
 public class fizzbuzztests {
 
-@Test public void TheFirstNumberInTheListIs1(){
-    FizzBuzzGame f = new FizzBuzzGame();
-    assert (f.getFizzBuzzResponse("1")=="1");
-}
-@Test public void TheNumber2Elicits2Response(){
-    FizzBuzzGame f = new FizzBuzzGame();
-    assert (f.getFizzBuzzResponse("2")=="2");
-}
-@Test public void TheNumber3ElicitsFizzResponse(){
-    FizzBuzzGame f = new FizzBuzzGame();
-    assert (f.getFizzBuzzResponse("3")=="fizz");
-}
-@Test public void TheNumber5ElicitsBuzzResponse(){
-    FizzBuzzGame f = new FizzBuzzGame();
-    assert (f.getFizzBuzzResponse("5")=="buzz");
-}
-@Test public void TheNumber15ElicitsFizzBuzzResponse(){
-    FizzBuzzGame f = new FizzBuzzGame();
-    assert (f.getFizzBuzzResponse("15")=="fizzbuzz");
-}
-
-
+    @Test public void TheFirstNumberInTheListIs1(){
+        FizzBuzzGame f = new FizzBuzzGame();
+        assert (f.getFizzBuzzResponse("1")=="1");
+    }
+    @Test public void TheNumber2Elicits2Response(){
+        FizzBuzzGame f = new FizzBuzzGame();
+        assert (f.getFizzBuzzResponse("2")=="2");
+    }
+    @Test public void TheNumber3ElicitsFizzResponse(){
+        FizzBuzzGame f = new FizzBuzzGame();
+        assert (f.getFizzBuzzResponse("3")=="fizz");
+    }
+    @Test public void TheNumber5ElicitsBuzzResponse(){
+        FizzBuzzGame f = new FizzBuzzGame();
+        assert (f.getFizzBuzzResponse("5")=="buzz");
+    }
+    @Test public void TheNumber15ElicitsFizzBuzzResponse(){
+        FizzBuzzGame f = new FizzBuzzGame();
+        assert (f.getFizzBuzzResponse("15")=="fizzbuzz");
+    }
 }
 
