d316b0149d817d7d5074f6a2ff2f1ffa0a7d653a
singicus <sandra@webcatgraphics.com>
2017-01-11 15:20:34 -0500

    Add files via upload

--- src/test/java/fizzbuzz/fizzbuzztests.java
+++ src/test/java/fizzbuzz/fizzbuzztests.java
@@ -5,23 +5,23 @@ import org.junit.*;
 public class fizzbuzztests {
 
 @Test public void TheFirstNumberInTheListIs1(){
-    fizzbuzz f = new fizzbuzz();
+    FizzBuzzGame f = new FizzBuzzGame();
     assert (f.getFizzBuzzResponse("1")=="1");
 }
 @Test public void TheNumber2Elicits2Response(){
-    fizzbuzz f = new fizzbuzz();
+    FizzBuzzGame f = new FizzBuzzGame();
     assert (f.getFizzBuzzResponse("2")=="2");
 }
 @Test public void TheNumber3ElicitsFizzResponse(){
-    fizzbuzz f = new fizzbuzz();
+    FizzBuzzGame f = new FizzBuzzGame();
     assert (f.getFizzBuzzResponse("3")=="fizz");
 }
 @Test public void TheNumber5ElicitsBuzzResponse(){
-    fizzbuzz f = new fizzbuzz();
+    FizzBuzzGame f = new FizzBuzzGame();
     assert (f.getFizzBuzzResponse("5")=="buzz");
 }
 @Test public void TheNumber15ElicitsFizzBuzzResponse(){
-    fizzbuzz f = new fizzbuzz();
+    FizzBuzzGame f = new FizzBuzzGame();
     assert (f.getFizzBuzzResponse("15")=="fizzbuzz");
 }
 
