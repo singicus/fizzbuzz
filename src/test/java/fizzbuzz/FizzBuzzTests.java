b1b1d6d832d834262ab373fde45efc233ea80691
Jeff Lieb <blockbattle@gmail.com>
2017-01-12 19:22:06 -0500

    Refactoring tests

--- src/test/java/fizzbuzz/FizzBuzzTests.java
+++ src/test/java/fizzbuzz/FizzBuzzTests.java
@@ -4,26 +4,25 @@ import org.junit.*;
 
 public class FizzBuzzTests {
 
-    @Test public void TheFirstNumberInTheListIs1(){
-        FizzBuzzGame f = new FizzBuzzGame();
-        assert (f.getFizzBuzzResponse("1")=="1");
-    }
-    @Test public void TheNumber2Elicits2Response(){
-        FizzBuzzGame f = new FizzBuzzGame();
-        assert (f.getFizzBuzzResponse("2")=="2");
-    }
-    @Test public void TheNumber3ElicitsFizzResponse(){
-        FizzBuzzGame f = new FizzBuzzGame();
-        assert (f.getFizzBuzzResponse("3")=="fizz");
-    }
-    @Test public void TheNumber5ElicitsBuzzResponse(){
-        FizzBuzzGame f = new FizzBuzzGame();
-        assert (f.getFizzBuzzResponse("5")=="buzz");
-    }
-    @Test public void TheNumber15ElicitsFizzBuzzResponse(){
-        FizzBuzzGame f = new FizzBuzzGame();
-        assert (f.getFizzBuzzResponse("15")=="fizzbuzz");
+    FizzBuzzGame subject = new FizzBuzzGame();
+
+    @Test public void TheFirstNumberInTheListIs1() {
+        assert (subject.getFizzBuzzResponse("1") == "1");
     }
 
+    @Test public void TheNumber2Elicits2Response() {
+        assert (subject.getFizzBuzzResponse("2") == "2");
+    }
+
+    @Test public void TheNumber3ElicitsFizzResponse() {
+        assert (subject.getFizzBuzzResponse("3") == "fizz");
+    }
+
+    @Test public void TheNumber5ElicitsBuzzResponse() {
+        assert (subject.getFizzBuzzResponse("5") == "buzz");
+    }
+
+    @Test public void TheNumber15ElicitsFizzBuzzResponse() {
+        assert (subject.getFizzBuzzResponse("15") == "fizzbuzz");
+    }
 }
-
